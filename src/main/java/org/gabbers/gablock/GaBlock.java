69863ea5ed49c4e4340a764ed7dc8e3c482d0d0c
GEGSGabriel <gabrielgonseg@gmail.com>
2023-08-10 16:11:30 -0600

    gradle error

--- src/main/java/org/gabbers/gablock/GaBlock.java
+++ src/main/java/org/gabbers/gablock/GaBlock.java
@@ -1,11 +1,14 @@
 package org.gabbers.gablock;
 import net.minecraft.world.item.BlockItem;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.CreativeModeTabs;
+import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.material.MapColor;
-import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
+import net.minecraftforge.fml.common.Mod;
 
 @Mod(GaBlock.MODID)
 public class GaBlock{
@@ -13,5 +16,5 @@ public class GaBlock{
 public static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS, MODID);
 public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, MODID);
 public static final RegistryObject<Block> SUPER_MEGA_GABLOCK = BLOCKS.register("super_mega_gablock", () -> new Block(BlockBehaviour.Properties.of().mapColor(MapColor.STONE)));
-public static final RegistryObject<Item> SUPER_MEGA_GABLOCK_ITEM = ITEMS.register("super_mega_gablock", () -> new BlockItem(EXAMPLE_BLOCK.get(), new Item.Properties()));
+public static final RegistryObject<Item> SUPER_MEGA_GABLOCK_ITEM = ITEMS.register("super_mega_gablock", () -> new BlockItem(SUPER_MEGA_GABLOCK.get(), new Item.Properties()));
 }
