16aa6c2ce5594af8e6e331394f1dc85022223115
Enrique Gonzalez <enriqueand22@outlook.com>
2023-09-14 21:17:39 -0600

    gablock nicely

--- src/main/java/org/gabbers/gablock/GaBlock.java
+++ src/main/java/org/gabbers/gablock/GaBlock.java
@@ -16,12 +16,22 @@ public class GaBlock{
   public static final String MODID = "gablock";
   public static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS, MODID);
   public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, MODID);
-  public static final RegistryObject<Block> SUPER_MEGA_GABLOCK = BLOCKS.register("super_mega_gablock", () -> new Block(BlockBehaviour.Properties.of().mapColor(MapColor.STONE)));
   public static final RegistryObject<Item> SUPER_MEGA_GABLOCK_ITEM = ITEMS.register("super_mega_gablock", () -> new BlockItem(SUPER_MEGA_GABLOCK.get(), new Item.Properties()));
 
   public static void register(IEventBus eventBus){
+    Block gablock = gaBlock();
+    BLOCKS.register("super_mega_gablock",() ->  gablock);
+
     BLOCKS.register(eventBus);
     ITEMS.register(eventBus);
+  }
+
+  public static Block gaBlock() {
+    BlockBehaviour.Properties props = BlockBehaviour.Properties.of();
+    props.mapColor(MapColor.COLOR_ORANGE);
+    Block block = new Block(props);
+
+    return block;
   }
 
   public GaBlock() {
